62d5416a93674f26d293a0e860fd0cce7a7ec4b1
mdouchement <marc.douchement@gmail.com>
2019-10-10 19:26:25 +0200

    Fix tests

--- internal/server/item_handler_test.go
+++ internal/server/item_handler_test.go
@@ -15,7 +15,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRequestItemsSync(t *testing.T) {
+type sync20161215 struct {
+	Retrieved     []*model.Item          `json:"retrieved_items"`
+	Saved         []*model.Item          `json:"saved_items"`
+	Unsaved       []*service.UnsavedItem `json:"unsaved"`
+	SyncToken     string                 `json:"sync_token"`
+	CursorToken   string                 `json:"cursor_token"`
+	IntegrityHash string                 `json:"integrity_hash,omitempty"`
+}
+
+func TestRequestItemsSync20161215(t *testing.T) {
 	engine, ioc, r, cleanup := setup()
 	defer cleanup()
 
@@ -66,7 +75,7 @@ func TestRequestItemsSync(t *testing.T) {
 	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 		assert.Equal(t, http.StatusOK, r.Code)
 
-		var v service.SyncService
+		var v sync20161215
 		err := json.Unmarshal(r.Body.Bytes(), &v)
 		assert.NoError(t, err)
 
@@ -94,7 +103,7 @@ func TestRequestItemsSync(t *testing.T) {
 	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 		assert.Equal(t, http.StatusOK, r.Code)
 
-		var v service.SyncService
+		var v sync20161215
 		err := json.Unmarshal(r.Body.Bytes(), &v)
 		assert.NoError(t, err)
 
@@ -112,7 +121,7 @@ func TestRequestItemsSync(t *testing.T) {
 	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 		assert.Equal(t, http.StatusOK, r.Code)
 
-		var v service.SyncService
+		var v sync20161215
 		err := json.Unmarshal(r.Body.Bytes(), &v)
 		assert.NoError(t, err)
 
@@ -129,7 +138,7 @@ func TestRequestItemsSync(t *testing.T) {
 	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 		assert.Equal(t, http.StatusOK, r.Code)
 
-		var v service.SyncService
+		var v sync20161215
 		err := json.Unmarshal(r.Body.Bytes(), &v)
 		assert.NoError(t, err)
 
@@ -140,5 +149,143 @@ func TestRequestItemsSync(t *testing.T) {
 		assert.Len(t, v.Retrieved, 2)
 		assert.Empty(t, v.Saved)
 		assert.Empty(t, v.Unsaved)
+	})
+}
+
+type sync20190520 struct {
+	Retrieved     []*model.Item           `json:"retrieved_items"`
+	Saved         []*model.Item           `json:"saved_items"`
+	Conflicts     []*service.ConflictItem `json:"conflicts"`
+	SyncToken     string                  `json:"sync_token"`
+	CursorToken   string                  `json:"cursor_token"`
+	IntegrityHash string                  `json:"integrity_hash,omitempty"`
+}
+
+func TestRequestItemsSync20190520(t *testing.T) {
+	engine, ioc, r, cleanup := setup()
+	defer cleanup()
+
+	r.POST("/items/sync").Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+		assert.JSONEq(t, `{"error":{"message":"missing or malformed jwt"}}`, r.Body.String())
+	})
+
+	user := createUser(ioc)
+	header := gofight.H{
+		"Authorization": "Bearer " + server.TokenFromUser(ioc, user),
+	}
+
+	r.POST("/items/sync").SetHeader(header).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+		assert.JSONEq(t, `{"error":{"message":"Could not get syncing params."}}`, r.Body.String())
+	})
+
+	//
+	//
+
+	item := &model.Item{
+		Base: model.Base{
+			ID: "d989ccc9-15c6-475e-839b-1690bd07d073",
+		},
+		UserID:           "b329a187-ddf8-4e9b-960d-49c272a58794",
+		Content:          "003:d83ea9b696c313c8a352795264873fefebbd60a92c5d9a89e3a380a0d3a68b62:d989ccc9-15c6-475e-839b-1690bd07d073:400591db0ad08c0847f45a1e76ceb87d:HI0PGPWB667YzIlWPR4A8VpGuDb9YOcTRknFb4CZXM2yJ0KK68W2giX6AV6KNV19exUvnunTmkfxlOWUfiG2m7YU2rIO76MMMfs5wqBKqO4eTuootiYVi5JbCW2BFHJcDnj3seb8juBV95Bm5lm4tQ==:eyJpZGVudGlmaWVyIjoiZ2VvcmdlLmFiaXRib2xAbm93aGVyZS5sYW4iLCJ2ZXJzaW9uIjoiMDAzIiwicHdfY29zdCI6NDIwMDAwLCJwd19ub25jZSI6Im5vbmNlIn0=",
+		ContentType:      libsf.ContentTypeNote,
+		EncryptedItemKey: "003:3c69d9526d2846671c7e8cf89968f3b6ffd92e82ca15b04d29a3f77100ce857c:d989ccc9-15c6-475e-839b-1690bd07d073:93b257d16f53732d81230e41b62eab7c:Ai0xyC1CFcah3/rubAXV+j433oXoABPU8kmYdAzE1WlscKQIXbds8USDG0HmoC1XkCHerozTcJc5IgTAN2JZZBYttmllRswgpn7vDKZIUbXa/FDao3l6a43hedxIfd+4b1moSnB1IgG/T8c+WoA0zDd5vKtB5EMyljLVbyItBZnNrg7toV1bSWQ1t+8xUcKm:eyJpZGVudGlmaWVyIjoiZ2VvcmdlLmFiaXRib2xAbm93aGVyZS5sYW4iLCJ2ZXJzaW9uIjoiMDAzIiwicHdfY29zdCI6NDIwMDAwLCJwd19ub25jZSI6Im5vbmNlIn0=",
+		Deleted:          false,
+	}
+	err := ioc.Database.Save(item)
+	if err != nil {
+		panic(err)
+	}
+
+	//
+	//
+
+	params := gofight.D{
+		"api":               "20190520", // ===========> !important
+		"compute_integrity": false,
+		"limit":             100000,
+		"sync_token":        "",
+		"cursor_token":      "",
+		"items":             []*model.Item{},
+	}
+
+	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		var v sync20190520
+		err := json.Unmarshal(r.Body.Bytes(), &v)
+		assert.NoError(t, err)
+
+		at := libsf.TimeFromToken(v.SyncToken)
+		assert.NotZero(t, at)
+		assert.WithinDuration(t, time.Now(), at, 100*time.Millisecond)
+
+		assert.Empty(t, v.Retrieved) // Nothing for this user
+		assert.Empty(t, v.Saved)
+		assert.Empty(t, v.Conflicts)
+	})
+
+	//
+	//
+
+	item.UserID = user.ID
+	err = ioc.Database.Save(item)
+	if err != nil {
+		panic(err)
+	}
+
+	//
+	//
+
+	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		var v sync20190520
+		err := json.Unmarshal(r.Body.Bytes(), &v)
+		assert.NoError(t, err)
+
+		at := libsf.TimeFromToken(v.SyncToken)
+		assert.NotZero(t, at)
+		assert.WithinDuration(t, time.Now(), at, 100*time.Millisecond)
+
+		assert.Len(t, v.Retrieved, 1)
+		assert.Empty(t, v.Saved)
+		assert.Empty(t, v.Conflicts)
+	})
+
+	item.SetID(uuid.Must(uuid.NewV4()).String())
+	params["items"] = []*model.Item{item}
+	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		var v sync20190520
+		err := json.Unmarshal(r.Body.Bytes(), &v)
+		assert.NoError(t, err)
+
+		at := libsf.TimeFromToken(v.SyncToken)
+		assert.NotZero(t, at)
+		assert.WithinDuration(t, time.Now(), at, 100*time.Millisecond)
+
+		assert.Len(t, v.Retrieved, 1)
+		assert.Len(t, v.Saved, 1)
+		assert.Empty(t, v.Conflicts)
+	})
+
+	params["items"] = []*model.Item{}
+	r.POST("/items/sync").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		var v sync20190520
+		err := json.Unmarshal(r.Body.Bytes(), &v)
+		assert.NoError(t, err)
+
+		at := libsf.TimeFromToken(v.SyncToken)
+		assert.NotZero(t, at)
+		assert.WithinDuration(t, time.Now(), at, 100*time.Millisecond)
+
+		assert.Len(t, v.Retrieved, 2)
+		assert.Empty(t, v.Saved)
+		assert.Empty(t, v.Conflicts)
 	})
 }
