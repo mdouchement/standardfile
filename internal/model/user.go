ae63da6d3fea1f8de3c160493b212e5254350ccd
mdouchement <marc.douchement@gmail.com>
2020-08-12 09:42:30 +0000

    Use a serializer to render the user values (#32)

--- internal/model/user.go
+++ internal/model/user.go
@@ -14,19 +14,18 @@ type User struct {
 	Base `msgpack:",inline" storm:"inline"`
 
 	// Standardfile fields
-	Email         string `json:"email"              msgpack:"email"    storm:"unique"`
-	Password      string `json:"-"                  msgpack:"password,omitempty"`
-	PasswordCost  int    `json:"pw_cost"            msgpack:"pw_cost"`
-	PasswordNonce string `json:"pw_nonce,omitempty" msgpack:"pw_nonce,omitempty"`
-	PasswordAuth  string `json:"pw_auth,omitempty"  msgpack:"pw_auth,omitempty"`
-	Version       string `json:"version"            msgpack:"version"`
+	Email         string `msgpack:"email"    storm:"unique"`
+	Password      string `msgpack:"password,omitempty"`
+	PasswordCost  int    `msgpack:"pw_cost"`
+	PasswordNonce string `msgpack:"pw_nonce,omitempty"`
+	PasswordAuth  string `msgpack:"pw_auth,omitempty"`
+	Version       string `msgpack:"version"`
 
 	// V2 flields compatibility
-	PasswordSalt string `json:"pw_salt,omitempty"  msgpack:"pw_salt,omitempty"`
+	PasswordSalt string `msgpack:"pw_salt,omitempty"`
 
 	// Custom fields
-	RegistrationPassword string `json:"password,omitempty" msgpack:"-"`
-	PasswordUpdatedAt    int64  `json:"-"                  msgpack:"password_updated_at"`
+	PasswordUpdatedAt int64 `msgpack:"password_updated_at"`
 }
 
 // NewUser returns a new user with default params.
