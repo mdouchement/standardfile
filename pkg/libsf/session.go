4c1fb9ec820c4b7b84f799b9750f5f5f3b7d1cae
cherya <ccherya@gmail.com>
2021-03-07 15:53:39 +0300

    [libsf] fix empty items sending and session expiration parsing

--- pkg/libsf/session.go
+++ pkg/libsf/session.go
@@ -1,6 +1,11 @@
 package libsf
 
-import "time"
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+	"time"
+)
 
 // A Session contains details about a session.
 type Session struct {
@@ -29,4 +34,42 @@ func (s Session) AccessExpired() bool {
 // RefreshExpired returns true if the refresh token is expired.
 func (s Session) RefreshExpired() bool {
 	return !s.Defined() || time.Now().After(s.RefreshExpiration)
+}
+
+type sessionTime time.Time
+
+func (t *sessionTime) UnmarshalJSON(s []byte) error {
+	r := strings.Replace(string(s), `"`, ``, -1)
+	tt, err := time.Parse(time.RFC3339, r)
+	if err != nil {
+		q, err := strconv.ParseInt(string(s), 10, 64)
+		if err != nil {
+			return err
+		}
+		*(*time.Time)(t) = time.Unix(q/1000, 0)
+		return nil
+	}
+	*(*time.Time)(t) = tt
+	return nil
+}
+
+func (s *Session) UnmarshalJSON(raw []byte) error {
+	st := &struct {
+		AccessToken       string      `json:"access_token"`
+		RefreshToken      string      `json:"refresh_token"`
+		AccessExpiration  sessionTime `json:"access_expiration"`
+		RefreshExpiration sessionTime `json:"refresh_expiration"`
+	}{}
+
+	err := json.Unmarshal(raw, st)
+	if err != nil {
+		return err
+	}
+
+	s.AccessToken = st.AccessToken
+	s.RefreshToken = st.RefreshToken
+	s.AccessExpiration = time.Time(st.AccessExpiration)
+	s.RefreshExpiration = time.Time(st.RefreshExpiration)
+
+	return nil
 }
