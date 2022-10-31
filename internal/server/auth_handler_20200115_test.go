5784717de5dcd543871166502ad8417bd722188d
Simon <26634807+Crusader99@users.noreply.github.com>
2022-10-31 15:49:11 +0100

    Fix e-mail and password change (#89)
    * Fix: Support for password change
    
    * Feat: Add support for email change
    
    * Test: Check email updates correct
    
    * Check passed id is the same like in bearer token

--- internal/server/auth_handler_20200115_test.go
+++ internal/server/auth_handler_20200115_test.go
@@ -455,4 +455,38 @@ func TestRequestUpdatePassword20200115(t *testing.T) {
 		assert.Equal(t, http.StatusUnauthorized, r.Code)
 		assert.JSONEq(t, `{"error":{"message":"The current password you entered is incorrect. Please try again."}}`, r.Body.String())
 	})
+
+	// If no email available, ensure no email updates
+	params["current_password"] = "yolo!"
+	r.PUT("/v1/users/"+user.ID+"/attributes/credentials").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		user, err := ctrl.Database.FindUser(user.ID) // reload user
+		assert.NoError(t, err)
+
+		assert.Equal(t, user.Email, "george.abitbol@nowhere.lan")
+	})
+
+	// If a new email is provided, check it's correctly updated
+	params["current_password"] = "yolo!"
+	params["new_email"] = "test@test.de"
+	r.PUT("/v1/users/"+user.ID+"/attributes/credentials").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, r.Code)
+
+		v, err := fastjson.Parse(r.Body.String())
+		assert.NoError(t, err)
+
+		user, err = ctrl.Database.FindUser(user.ID) // reload user
+		assert.NoError(t, err)
+
+		assert.Equal(t, user.Email, string(v.Get("user", "email").GetStringBytes()))
+		assert.Equal(t, user.Email, params["new_email"])
+	})
+
+	// If the user id parameter is different from the request's bearer token
+	params["current_password"] = "yolo!"
+	params["new_email"] = "test@test.de"
+	r.PUT("/v1/users/DIFFERENT-ID-THAN-IN-BEARER-TOKEN/attributes/credentials").SetHeader(header).SetJSON(params).Run(engine, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusUnauthorized, r.Code)
+	})
 }
