ce45445e63b04bdb4e3bd4a1de7ab733cb21c069
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2022-11-04 00:32:06 +0100

    Fix: Correct error handling for file-api

--- internal/server/files.go
+++ internal/server/files.go
@@ -5,11 +5,13 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -17,6 +19,28 @@ type files struct{}
 
 type Resource struct {
 	RemoteIdentifier string `json:"remoteIdentifier"`
+}
+
+func respond(context echo.Context, code int, message string) error {
+	success := code < 300
+	return context.JSON(code, echo.Map{
+		"success": success,
+		"message": message,
+	})
+}
+
+func readValetToken(context echo.Context) (*ValetToken, error) {
+	valetTokenBase64 := context.Request().Header.Get("x-valet-token")
+	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	if err != nil {
+		return nil, errors.New("Unable to parse base64 valet token")
+	}
+	valetTokenJson := string(valetTokenBytes)
+	var token ValetToken
+	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
+		return nil, errors.New("Unable to parse json valet token")
+	}
+	return &token, nil
 }
 
 type ValetRequestParams struct {
@@ -29,26 +53,29 @@ type ValetToken struct {
 	FileID        string `json:"fileId"`
 }
 
-func (token *ValetToken) GetFilePath() string {
-	// TODO: Check format of FileID (Security)
+func (token *ValetToken) getFilePath() (*string, error) {
+	id, err := uuid.FromString(token.FileID)
+	if err != nil {
+		return nil, errors.New("Unable to parse json valet token")
+	} else if !fs.ValidPath(id.String()) {
+		return nil, errors.New("Invalid path")
+	}
 	// TODO: Allow custom path in config
 	// TODO: Subfolders for each user (Compatible format with official server)
-	// TODO: use filepath.Join function
-	// TODO: Fobbid "../../" pattern, may be with https://pkg.go.dev/io/fs#ValidPath
-	return "/etc/standardfile/database/" + token.FileID
+	path := filepath.Join("etc", "standardfile", "database", id.String())
+	return &path, nil
 }
 
 // Provides a valet token that is required to execute an operation
 func (h *files) ValetTokens(c echo.Context) error {
 	var params ValetRequestParams
 	if err := c.Bind(&params); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, "Unable to parse request")
+	} else if len(params.Resources) != 1 {
+		return respond(c, http.StatusBadRequest, "Multi file requests unsupported")
 	}
 
-	if len(params.Resources) != 1 {
-		return c.JSON(http.StatusBadRequest, "Multi file requests not supported")
-	}
-
+	// Generate valet token. Used for actual file operations
 	var token ValetToken
 	token.Authorization = c.Request().Header.Get(echo.HeaderAuthorization)
 	token.FileID = params.Resources[0].RemoteIdentifier
@@ -56,7 +83,6 @@ func (h *files) ValetTokens(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
-
 	return c.JSON(http.StatusOK, echo.Map{
 		"success":    true,
 		"valetToken": base64.StdEncoding.EncodeToString(valetTokenJson),
@@ -65,21 +91,19 @@ func (h *files) ValetTokens(c echo.Context) error {
 
 // Called before uploading chunks of a file
 func (h *files) CreateUploadSession(c echo.Context) error {
-	valetTokenBase64 := c.Request().Header.Get("x-valet-token")
-	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	token, err := readValetToken(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-	valetTokenJson := string(valetTokenBytes)
-
-	var token ValetToken
-	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
 
-	fmt.Println("create-session. valet_token: " + valetTokenJson)
+	// Validate file path
+	path, err := token.getFilePath()
+	if err != nil {
+		return respond(c, http.StatusBadRequest, err.Error())
+	}
 
-	if _, err := os.Create(token.GetFilePath()); err != nil {
+	// Create empty file
+	if _, err := os.Create(*path); err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
 
@@ -91,96 +115,76 @@ func (h *files) CreateUploadSession(c echo.Context) error {
 
 // Called when uploaded all chunks of a file
 func (h *files) CloseUploadSession(c echo.Context) error {
-	valetTokenBase64 := c.Request().Header.Get("x-valet-token")
-	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	token, err := readValetToken(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
-	valetTokenJson := string(valetTokenBytes)
-	var token ValetToken
-	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	} else if _, err := os.Stat(token.GetFilePath()); errors.Is(err, os.ErrNotExist) {
-		return c.JSON(http.StatusBadRequest, "File not created")
+	path, err := token.getFilePath()
+	if err != nil {
+		return respond(c, http.StatusBadRequest, err.Error())
+	} else if _, err := os.Stat(*path); errors.Is(err, os.ErrNotExist) {
+		return respond(c, http.StatusBadRequest, "File not created")
 	}
-
-	fmt.Println("close-session. valet_token: " + valetTokenJson)
-	return c.JSON(http.StatusOK, echo.Map{
-		"success": true,
-		"message": "File uploaded successfully",
-	})
+	return respond(c, http.StatusOK, "File uploaded successfully")
 }
 
 // Upload parts of a file in an existing session
 func (h *files) UploadChunk(c echo.Context) error {
-	valetTokenBase64 := c.Request().Header.Get("x-valet-token")
-	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	token, err := readValetToken(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
-	valetTokenJson := string(valetTokenBytes)
-	var token ValetToken
-	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	} else if _, err := os.Stat(token.GetFilePath()); errors.Is(err, os.ErrNotExist) {
+
+	// Validate file path
+	path, err := token.getFilePath()
+	if err != nil {
+		return respond(c, http.StatusBadRequest, err.Error())
+	} else if _, err := os.Stat(*path); errors.Is(err, os.ErrNotExist) {
 		return c.JSON(http.StatusBadRequest, "File not created")
 	}
 
-	chunk_id := c.Request().Header.Get("x-chunk-id")
-	fmt.Println("chunk. valet_token: " + valetTokenJson + " chunk_id: " + chunk_id)
-
-	f, err := os.OpenFile(token.GetFilePath(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	// Open file
+	f, err := os.OpenFile(*path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, "Unable to open file")
 	}
 	defer f.Close()
 
-	// Create new buffer
+	// Append chunk to file via buffer
 	writer := bufio.NewWriter(f)
 	reader := c.Request().Body
 	if _, err := io.Copy(writer, reader); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, "Unable to store file")
 	}
-
-	return c.JSON(http.StatusOK, echo.Map{
-		"success": true,
-		"message": "Chunk uploaded successfully",
-	})
+	return respond(c, http.StatusOK, "Chunk uploaded successfully")
 }
 
 // Delete file from server
 func (h *files) Delete(c echo.Context) error {
-	valetTokenBase64 := c.Request().Header.Get("x-valet-token")
-	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	token, err := readValetToken(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
-	valetTokenJson := string(valetTokenBytes)
-	var token ValetToken
-	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-	err = os.Remove(token.GetFilePath())
+	path, err := token.getFilePath()
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
+	} else if err := os.Remove(*path); err != nil {
+		return respond(c, http.StatusBadRequest, "Unable to remove file")
 	}
-	return c.JSON(http.StatusOK, echo.Map{
-		"success": true,
-		"message": "File removed successfully",
-	})
+	return respond(c, http.StatusOK, "File removed successfully")
 }
 
 // Send encrypted file to client
 func (h *files) Download(c echo.Context) error {
-	valetTokenBase64 := c.Request().Header.Get("x-valet-token")
-	valetTokenBytes, err := base64.StdEncoding.DecodeString(valetTokenBase64)
+	token, err := readValetToken(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
-	valetTokenJson := string(valetTokenBytes)
-	var token ValetToken
-	if err := json.Unmarshal([]byte(valetTokenJson), &token); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+
+	// Validate file path
+	path, err := token.getFilePath()
+	if err != nil {
+		return respond(c, http.StatusBadRequest, err.Error())
 	}
-	return c.File(token.GetFilePath())
+	return c.File(*path)
 }
